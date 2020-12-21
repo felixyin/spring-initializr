2dd39e37a3fd788313440210d5ae99751b7405d3
尹彬 <1052921694@qq.com>
2020-12-21 11:48:32 +0800

    Initial commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+mkdir -p ~/tools/
+mkdir -p ~/bin/
+
+cp -r ../spring-initializr ~/tools/
+
+ln -s ~/tools/spring-initializr/start.sh ~/bin/si
+
+echo "提示: echo export PATH=$PATH:~/bin"
+echo "使用： si"
---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# nohup java -jar ./start-site-exec.jar  &
+
+# tail -n 1000 -f nohup.out
+
+java -jar ~/tools/spring-initializr/start-site-exec.jar
